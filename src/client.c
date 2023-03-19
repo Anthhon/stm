58a655f25a473e77af26983cdd14e47e0f4b2def
Anthhon <anthcomercial@gmail.com>
2023-03-19 14:20:26 -0300

    tcp chat working

--- src/client.c
+++ src/client.c
@@ -11,39 +11,49 @@
 #define END_COLOR "\033[0m"
 
 #define BUFFER_SIZE 4096
+#define MAX_MESSAGE_SIZE 256
 #define TERMINATOR 1
 #define PORT 8080
+#define TRUE 1
+#define FALSE 0
 
-int main(int argc, char const *argv[])
-{
-	struct sockaddr_in address;
-	char buffer[BUFFER_SIZE] = { 0 };
-	char username[] = "Anthhon";	
-	char message[] = "Hello server, that's my test message";	
-	int socket_handler, status, read_status;
+void chat_start(int cli_socket){
+	char buffer[MAX_MESSAGE_SIZE];
+	size_t buffer_t = sizeof(buffer);
 
-	/* Assign username to the message */
-	char *f_message = malloc(sizeof(*f_message) * (strlen(username) + strlen(message) + TERMINATOR));	
-	(void)sprintf(f_message, "%s: %s", username, message);
+	while (TRUE){
+		/* Read client message, insert into buffer and sent it */
+		int i = 0;
+		(void)fprintf(stdout, "Type your message: ");
+		while ((buffer[i++] = getchar()) != '\n');
+		write(cli_socket, buffer, buffer_t);		
 
-	if (argc != 2){
-		(void)fprintf(stdout, "Usage: %s [server_ip]\n", argv[0]);
-		exit(EXIT_FAILURE);
+		/* Read and print-out receive message */
+		(void)memset(buffer, 0, buffer_t);
+		(void)read(cli_socket, buffer, buffer_t);
+		(void)fprintf(stdout, "From server: %s", buffer);
+		(void)memset(buffer, 0, buffer_t);
+
+		/* Check if msg contains "Exit" then quit server */
+		if (strcmp("Exit", buffer) == 0){
+			(void)fprintf(stdout, "Exiting server...");
+			break;
+		}
 	}
+}
 
+void socket_build(const char *IP){
+	struct sockaddr_in address;
+	int socket_handler, status;
+	
 	/* Creating socket descriptor */
 	if ((socket_handler = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		(void)fprintf(stderr, "ERROR: socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr(IP);
 	address.sin_port = htons(PORT);
-
-	/* Convert IPv4 and IPv6 addresses from text to binary */	
-	if (inet_pton(AF_INET, argv[1], &address.sin_addr) <= 0){
-		(void)fprintf(stderr, "ERROR: addresses conversions failed");
-		exit(EXIT_FAILURE);
-	}
 
 	/* Initiate connection on a socket */
 	if ((status = connect(socket_handler, 
@@ -52,15 +62,20 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	(void)send(socket_handler, f_message, strlen(f_message), 0);	
-	if ((read_status = read(socket_handler, buffer, BUFFER_SIZE)) == -1){
-		(void)fprintf(stderr, "ERROR: message reading failed");
-		exit(EXIT_FAILURE);
-	};
-	(void)fprintf(stdout, "%s\n", buffer);
+	chat_start(socket_handler);
 
 	/* Closing the connected socket */
 	(void)close(socket_handler);
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 3){
+		(void)fprintf(stdout, "Usage: %s [server_ip] [username]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	socket_build(argv[1]);	
 
 	return(0);
 }
--- src/server.c
+++ src/server.c
@@ -10,70 +10,80 @@
 #define INIT_GREEN "\033[0;31m"
 #define END_COLOR "\033[0m"
 
-#define PORT 8080
 #define BUFFER_SIZE 4096
+#define MAX_MESSAGE_SIZE 256
 #define TERMINATOR 1
+#define TRUE 1
+#define FALSE 0
 
-int main(int agrc, char const *argv[])
+void chat_start(int cli_socket)
 {
-	struct sockaddr_in address;
-	char buffer[BUFFER_SIZE] = { 0 };
-	char server_name[] = "SERVER";	
-	char message[] = "Sending testing message";
-	int socket_handler, new_socket, read_status;
-	int addrlen = sizeof(address);
-	int backlog_len = 3;
-	int opt = 1;
+	char buffer[MAX_MESSAGE_SIZE];
+	size_t buffer_t = sizeof(buffer);
 
-	/* Assign server name to message */
-	char *f_message = malloc(sizeof(*f_message) * (strlen(server_name) + strlen(message) + TERMINATOR));
-	(void)sprintf(f_message, "%s: %s", server_name, message); 
+	while (TRUE){
+		/* Read and print-out receive message */
+		(void)memset(buffer, 0, buffer_t);
+		(void)read(cli_socket, buffer, buffer_t);
+		(void)fprintf(stdout, "From client: %s", buffer);
+		(void)memset(buffer, 0, buffer_t);
+
+		/* Read server message, insert into buffer and sent it */
+		int i = 0;
+		(void)fprintf(stdout, "Type your message: ");
+		while ((buffer[i++] = getchar()) != '\n');
+		write(cli_socket, buffer, buffer_t);
+
+		/* Check if msg contains "Exit" then quit server */
+		if (strcmp("Exit", buffer) == 0){
+			(void)fprintf(stdout, "Exiting server...");
+			break;
+		}
+	}
+}
+
+int main()
+{
+	struct sockaddr_in address, cli;
+	const int SERVER_PROTOCOL = AF_INET;
+	const int BACKLOG_LEN = 3;
+	const int PORT = 8080;
+	const int CLI_LEN = sizeof(cli);
+	int socket_handler;
+	int new_socket;
 
 	/* Creating socket descriptor */
-	if ((socket_handler = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+	if ((socket_handler = socket(SERVER_PROTOCOL, SOCK_STREAM, 0)) == -1){
 		(void)fprintf(stderr, "ERROR: socket creation failed");
 		exit(EXIT_FAILURE);
 	}
+	bzero(&address, sizeof(address)); /* Fill address with 0's */
 
-	/* Initializing socket */	
-	if (setsockopt(socket_handler, SOL_SOCKET,
-		       SO_REUSEADDR | SO_REUSEPORT,
-		       &opt, sizeof(opt))){
-		(void)fprintf(stderr, "ERROR: socket operations setting failed");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_family = SERVER_PROTOCOL;
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
 	address.sin_port = htons(PORT);
 
-	/* Attach socket to port 8080 */
+	/* Attach socket to given IP and PORT */
 	if (bind(socket_handler, (struct sockaddr*)&address, sizeof(address)) == -1){
 		(void)fprintf(stderr, "ERROR: socket binding at %d failed", PORT);
 		exit(EXIT_FAILURE);
 	}
-	if (listen(socket_handler, backlog_len) == -1){
+	if (listen(socket_handler, BACKLOG_LEN) == -1){
 		(void)fprintf(stderr, "ERROR: socket listening failed");
 		exit(EXIT_FAILURE);
 	}
+
 	if ((new_socket = accept(socket_handler,
-			  (struct sockaddr*)&address,
-			  (socklen_t*)&addrlen)) == -1){
-		(void)fprintf(stderr, "ERROR: failed to accept new socket");
+			  (struct sockaddr*)&cli,
+			  (socklen_t*)&CLI_LEN)) == -1){
+		(void)fprintf(stderr, "ERROR: failed to accept new client");
 		exit(EXIT_FAILURE);
 	}
 
-	/* Read socket handler */
-	if ((read_status = read(new_socket, buffer, BUFFER_SIZE)) == -1){
-		(void)fprintf(stderr, "ERROR: message reading failed");
-		exit(EXIT_FAILURE);
-	}
-	(void)fprintf(stdout, "%s\n", buffer);
-	(void)send(new_socket, f_message, strlen(f_message), 0);
+	chat_start(new_socket);	
 
-	/* Ending the connected socket */
+	/* Close open socket after chatting */
 	(void)close(new_socket);
-	/* Ending the listening socket */
-	(void)shutdown(socket_handler, SHUT_RDWR);
 
 	return 0;
 }
